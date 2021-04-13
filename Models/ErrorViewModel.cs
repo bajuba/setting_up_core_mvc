060b144f5ef7d7c43f1207e4ba4f0cd643906c87
James Munger <jmunger@lakelandcollege.edu>
2021-04-13 14:25:31 -0500

    Initial Setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace setting_up_core_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
